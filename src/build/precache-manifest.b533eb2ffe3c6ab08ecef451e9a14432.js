87baa63dc2967d7c245899091585dbd10f899238
Adam Farrell <adamfarrell@weber.edu>
2019-08-19 14:49:43 -0600

    First commit

---
+++ src/build/precache-manifest.b533eb2ffe3c6ab08ecef451e9a14432.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445d8c70266c15678206be23257c09ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f04bb4263032ee7673d1",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "49c25252228585af81e6",
+    "url": "/static/js/2.3e78a732.chunk.js"
+  },
+  {
+    "revision": "f04bb4263032ee7673d1",
+    "url": "/static/js/main.f6397417.chunk.js"
+  },
+  {
+    "revision": "590b1f1a44e44eae64fb",
+    "url": "/static/js/runtime~main.8f66630e.js"
+  }
+]);
